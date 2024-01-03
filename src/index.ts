650778ce187a99166f975fcfe6bce44c1127d40e
aName2050 <shafaeebardia@gmail.com>
2024-01-02 18:56:01 -0800

    added test for reading .g .gsx and .gsc files

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import fs from 'fs';
-import { tokenize } from './GigaScript/lexer/lexer';
 
-const testSource = fs.readFileSync('./src/tests/main.g', { encoding: 'utf-8' });
-for (const token of tokenize(testSource)) {
-    console.log(token);
-}
+import Parser from './GigaScript/parser/parser';
+import { evaluate } from './GigaScript/runtime/interpreter';
+
+const g = fs.readFileSync('./src/tests/main.g', { encoding: 'utf-8' }); // standard GigaScript file
+const gsx = fs.readFileSync('./src/tests/main.gsx', { encoding: 'utf-8' }); // gen-z slang version of GigaScript
+// coming soon!
+// const gsc = fs.readFileSync('./src/tests/main.gsc', {encoding: 'utf-8'}); // compiled version of .g file
+
+const parser = new Parser();
+const program = parser.generateAST(g);
+const result = evaluate(program);
+console.log(result);
