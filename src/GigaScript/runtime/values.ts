caf579be99ae1dc492f9a72d87821c61a6599b2d
aName <shafaeebardia@gmail.com>
2024-01-09 19:46:45 +0000

    added undefined value

--- src/GigaScript/runtime/values.ts
+++ src/GigaScript/runtime/values.ts
@@ -1,87 +1,98 @@
-import { Stmt } from '../ast/ast';
-import Environment from './environment';
+import { Stmt } from "../ast/ast";
+import Environment from "./environment";
 
 export type ValueType =
-    | 'null'
-    | 'number'
-    | 'boolean'
-    | 'object'
-    | 'nativeFunction'
-    | 'function';
+	| "undefined"
+	| "NaN"
+	| "null"
+	| "number"
+	| "boolean"
+	| "object"
+	| "nativeFunction"
+	| "function";
 
 export interface RuntimeValue {
-    type: ValueType;
+	type: ValueType;
 }
 
 /**
  * A value of no meaning
  */
 export interface NullValue extends RuntimeValue {
-    type: 'null';
-    value: null;
+	type: "null";
+	value: null;
 }
 
 export function NULL() {
-    return { type: 'null', value: null } as NullValue;
+	return { type: "null", value: null } as NullValue;
+}
+
+export interface UndefinedValue extends RuntimeValue {
+	type: "undefined";
+	value: undefined;
+}
+
+export function UNDEFINED() {
+	return { type: "undefined", value: undefined } as UndefinedValue;
 }
 
 /**
  * Runtime value with direct access to the raw JavaScript boolean
  */
 export interface BooleanValue extends RuntimeValue {
-    type: 'boolean';
-    value: boolean;
+	type: "boolean";
+	value: boolean;
 }
 
 export function BOOL(b = true) {
-    return { type: 'boolean', value: b } as BooleanValue;
+	return { type: "boolean", value: b } as BooleanValue;
 }
 
 /**
  * Runtime value with direct access to the raw JavaScript number
  */
 export interface NumberValue extends RuntimeValue {
-    type: 'number';
-    value: number;
+	type: "number";
+	value: number;
 }
 
 export function NUMBER(n = 0) {
-    return { type: 'number', value: n } as NumberValue;
+	return { type: "number", value: n } as NumberValue;
 }
 
 export interface ObjectValue extends RuntimeValue {
-    type: 'object';
-    properties: Map<string, RuntimeValue>;
+	type: "object";
+	properties: Map<string, RuntimeValue>;
 }
 
 export function OBJECT(obj: Map<string, RuntimeValue>) {
-    return { type: 'object', properties: obj } as ObjectValue;
+	return { type: "object", properties: obj } as ObjectValue;
 }
 
 /**
  * Runtime value for function calls
  */
 export type FunctionCall = (
-    args: RuntimeValue[],
-    env: Environment
+	args: RuntimeValue[],
+	env: Environment
 ) => RuntimeValue;
 
 /**
  * Runtime value for native functions
  */
 export interface NativeFunctionValue extends RuntimeValue {
-    type: 'nativeFunction';
-    call: FunctionCall;
+	type: "nativeFunction";
+	call: FunctionCall;
 }
 
 export function NATIVE_FUNCTION(call: FunctionCall) {
-    return { type: 'nativeFunction', call } as NativeFunctionValue;
+	return { type: "nativeFunction", call } as NativeFunctionValue;
 }
 
 export interface FunctionValue extends RuntimeValue {
-    type: 'function';
-    name: string;
-    parameters: string[];
-    declarationEnv: Environment;
-    body: Stmt[];
+	type: "function";
+	name: string;
+	parameters: string[];
+	declarationEnv: Environment;
+	body: Stmt[];
 }
