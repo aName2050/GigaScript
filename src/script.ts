8a9ad6ca9e03fc84f331cc27d4b49f8e9b21483f
aName <shafaeebardia@gmail.com>
2024-01-16 20:08:46 +0000

    added gigascript run script to package.json

---
+++ src/script.ts
@@ -0,0 +1,57 @@
+import fs from "fs";
+import repl from "repl";
+
+import Parser from "./GigaScript/parser/parser";
+import { evaluate } from "./GigaScript/runtime/interpreter";
+import { createGlobalScope } from "./GigaScript/runtime/environment";
+
+const file = process.argv[2];
+
+if (file) {
+	// run file
+	run(file);
+} else {
+	// start repl
+	const v = "v1";
+	console.log(`GigaScript REPL ${v}\n`);
+
+	repl.start({ prompt: "> ", eval: handle });
+}
+
+function run(filename: string) {
+	const parser = new Parser();
+	const env = createGlobalScope();
+
+	let file = fs.readFileSync(filename, "utf-8");
+
+	if (file.endsWith(".g")) {
+		// handle standard GigaScript files
+		const program = parser.generateAST(file);
+		const res = evaluate(program, env);
+
+		return res;
+	} else if (file.endsWith(".gsx")) {
+		// handle gen-z GigaScript files
+		console.log(".gsx files are not yet supported");
+		process.exit(1);
+	} else {
+		throw `File does not end with ".g" or ".gsx". ${
+			file.split(".")[1]
+		} is not a supported file type.`;
+	}
+}
+
+function handle(
+	uInput: string,
+	_context: unknown,
+	_filename: unknown,
+	callback: any
+): void {
+	const parser = new Parser();
+	const env = createGlobalScope();
+
+	const program = parser.generateAST(uInput);
+	evaluate(program, env);
+
+	callback();
+}
--- src/tests/repl.ts
+++ src/tests/repl.ts
@@ -1,12 +1,10 @@
-import repl from 'repl';
+import repl from "repl";
 
-import Parser from '../GigaScript/parser/parser';
-import { createGlobalScope } from '../GigaScript/runtime/environment';
-import { evaluate } from '../GigaScript/runtime/interpreter';
+import Parser from "../GigaScript/parser/parser";
+import { createGlobalScope } from "../GigaScript/runtime/environment";
+import { evaluate } from "../GigaScript/runtime/interpreter";
 
-import { MK_NULL, MK_NUMBER, MK_BOOL } from '../GigaScript/runtime/values';
-
-const v = 'v1';
+const v = "v1";
 console.log(`REPL ${v}\n`);
 const r = repl.start({ prompt: `> `, eval: handle });
 
@@ -14,15 +12,15 @@ const parser = new Parser();
 const env = createGlobalScope();
 
 function handle(
-    uInput: string,
-    context: unknown,
-    filename: unknown,
-    callback: any
+	uInput: string,
+	context: unknown,
+	filename: unknown,
+	callback: any
 ) {
-    const program = parser.generateAST(uInput);
-    evaluate(program, env);
+	const program = parser.generateAST(uInput);
+	evaluate(program, env);
 
-    callback();
+	callback();
 }
 
 export default r;
