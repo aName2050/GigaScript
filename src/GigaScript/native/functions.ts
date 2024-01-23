15cb93b568fa6dc9a7d12c1c9bd25a4e7e677c66
aName <shafaeebardia@gmail.com>
2024-01-23 20:32:24 +0000

    added more native math functions

--- src/GigaScript/native/functions.ts
+++ src/GigaScript/native/functions.ts
@@ -1,57 +1,79 @@
 import {
-    BooleanValue,
-    FunctionValue,
-    NATIVE_FUNCTION,
-    NULL,
-    NUMBER,
-    NullValue,
-    NumberValue,
-    OBJECT,
-    ObjectValue,
-    RuntimeValue,
-    STRING,
-    StringValue,
+	BooleanValue,
+	FunctionValue,
+	NATIVE_FUNCTION,
+	NULL,
+	NUMBER,
+	NullValue,
+	NumberValue,
+	OBJECT,
+	ObjectValue,
+	RuntimeValue,
+	STRING,
+	StringValue,
 } from '../runtime/values';
 import { getValue } from '../util/util';
 
 export const print = NATIVE_FUNCTION((args, scope) => {
-    const output: any[] = [];
+	const output: any[] = [];
 
-    for (const arg of args) {
-        output.push(getValue(arg));
-    }
+	for (const arg of args) {
+		output.push(getValue(arg));
+	}
 
-    console.log(...output);
+	console.log(...output);
 
-    return NULL();
+	return NULL();
 });
 
 export const timestamp = NATIVE_FUNCTION((args, scope) => {
-    return NUMBER(Date.now());
+	return NUMBER(Date.now());
 });
 
 export const math = OBJECT(
-    new Map().set('pi', NUMBER(Math.PI)).set(
-        'sqrt',
-        NATIVE_FUNCTION((args, _scope) => {
-            const num = (args[0] as NumberValue).value;
-            return NUMBER(Math.sqrt(num));
-        })
-    )
+	new Map()
+		.set('pi', NUMBER(Math.PI))
+		.set('e', NUMBER(Math.E))
+		.set(
+			'sqrt',
+			NATIVE_FUNCTION((args, _scope) => {
+				const num = (args[0] as NumberValue).value;
+				return NUMBER(Math.sqrt(num));
+			})
+		)
+		.set(
+			'abs',
+			NATIVE_FUNCTION((args, _scope) => {
+				const num = (args[0] as NumberValue).value;
+				return NUMBER(Math.abs(num));
+			})
+		)
+		.set(
+			'round',
+			NATIVE_FUNCTION((args, _scope) => {
+				const num = (args[0] as NumberValue).value;
+				return NUMBER(Math.round(num));
+			})
+		)
+        .set('random', NATIVE_FUNCTION((args, _scope) => {
+            const max = Math.floor((args[0] as NumberValue).value);
+            const min = Math.ceil((args[0] as NumberValue).value);
+            return NUMBER(Math.random() * (max - min + 1) + min);
+        }))
 );
 
 export const format = NATIVE_FUNCTION((args, scope) => {
-    const str = args.shift() as StringValue;
+	const str = args.shift() as StringValue;
 
-    let res = '';
+	let res = '';
 
-    for (let i = 0; i < args.length; i++) {
-        const arg = args[i] as StringValue;
+	for (let i = 0; i < args.length; i++) {
+		const arg = args[i] as StringValue;
 
-        res = str.value.replace(/\${}/, arg.value);
-    }
+		res = str.value.replace(/\${}/, arg.value);
+	}
 
-    if (!args[0]) throw '"format" expected 2 arguments, but got 1.';
+	if (!args[0]) throw '"format" expected 2 arguments, but got 1.';
 
-    return STRING(res);
+	return STRING(res);
 });
