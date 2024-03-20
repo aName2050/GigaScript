8981375d8be69291e1ff56718b40eba48e546fc7
aName <shafaeebardia@gmail.com>
2024-03-20 18:56:35 +0000

    added all gigasscript types

--- src/GigaScript/runtime/types.ts
+++ src/GigaScript/runtime/types.ts
@@ -1,3 +1,6 @@
+import Environment from '../../../old/GigaScript/runtime/environment';
+import { STATEMENT } from '../ast/ast';
+
 export type DataType =
 	| 'undefined'
 	| 'null'
@@ -27,4 +30,42 @@ export function NULL(): Value<'null', null> {
 
 export function NUMBER(n = 0): Value<'number', number> {
 	return { type: 'number', value: n } as Value<'number', number>;
+}
+
+export function BOOLEAN(b = false): Value<'boolean', boolean> {
+	return { type: 'boolean', value: b } as Value<'boolean', boolean>;
+}
+
+export function OBJECT(
+	obj: Map<string, Value<DataType, any>>
+): Value<'object', object> {
+	return { type: 'object', value: obj } as Value<'object', object>;
+}
+
+export function ARRAY(
+	arr: Array<Value<DataType, any>>
+): Value<'array', Array<any>> {
+	return { type: 'array', value: arr } as Value<'array', Array<any>>;
+}
+
+export type FunctionCall = (
+	args: Array<Value<DataType, any>>
+	// env: Environment
+) => Value<DataType, any>;
+
+export interface NativeFnVal {
+	type: 'nativeFn';
+	call: FunctionCall;
+}
+
+export function NATIVEFN(call: FunctionCall): NativeFnVal {
+	return { type: 'nativeFn', call } as NativeFnVal;
+}
+
+export interface FuncVal {
+	type: 'function';
+	name: string;
+	params: Array<string>;
+	// decEnv: Environment;
+	body: Array<STATEMENT>;
 }
