6144f0ee606e8118be13d926e5432be3cf8ee094
aName <shafaeebardia@gmail.com>
2024-11-08 20:31:48 +0000

    updated AST tree visualizer (buggy; WIP)

--- tests/script.js
+++ tests/script.js
@@ -5,10 +5,13 @@ var clear = document.getElementById('clear');
 
 var container = document.getElementById('ast-container');
 
+var HTML_BUILD_STARTED = false;
+
 btn.addEventListener(
 	'click',
 	function () {
 		if (!txtarea.value) return alert('an input is required');
+		HTML_BUILD_STARTED = true;
 		container.innerHTML = buildHTMLFromAST(JSON.parse(txtarea.value));
 	},
 	false
@@ -19,11 +22,55 @@ clear.addEventListener(
 	function () {
 		container.innerHTML = '';
 		txtarea.value = '';
+		HTML_BUILD_STARTED = false;
 	},
 	false
 );
 
-function buildHTMLFromAST(ast) {
-	alert('not implemented');
-	throw 'not implemented';
+var OPEN_BRACE = '&#123;';
+var CLOSE_BRACE = '&#125;';
+var OPEN_BRACKET = '&#91;';
+var CLOSE_BRACKET = '&#93;';
+
+function buildHTMLFromAST(rawAST) {
+	var html = '<ul>';
+	html += `
+	<li class="entry toggleable open">
+		<span class="value" id="program">Program</span>
+		<span class="prefix p">${OPEN_BRACE}</span>
+		<ul class="value-body">`;
+
+	for (var key in rawAST) {
+		if (typeof rawAST[key] == 'object') {
+			var nodeIsArray = Array.isArray(rawAST[key]);
+
+			if (nodeIsArray) {
+				// if there is an array of nodes within
+				// the current node
+				// TODO:
+			} else {
+				// if the node is just an object
+				html += buildHTMLFromAST(rawAST[key]);
+			}
+		} else {
+			// handle keys with only a simple value
+			html += `
+			<li class="entry">
+				<span class="key">
+					<span class="name">${key}</span>
+					<span class="p">:&nbsp;</span>
+				</span>
+				<span class="value">
+					<span class="s">"${rawAST[key]}"</span>
+				</span>
+			</li>`;
+		}
+	}
+
+	html += `
+		</ul>
+		<span class="suffix p">${CLOSE_BRACE}</span>
+	</li>
+</ul>`;
+	return html;
 }
