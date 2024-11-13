8bfb937d7c08b43996318e4a3aee79b0dd282daa
aName <shafaeebardia@gmail.com>
2024-11-13 19:53:17 +0000

    finished framework for AST visualizer

--- tests/script.js
+++ tests/script.js
@@ -12,7 +12,17 @@ btn.addEventListener(
 	function () {
 		if (!txtarea.value) return alert('an input is required');
 		HTML_BUILD_STARTED = true;
-		container.innerHTML = buildHTMLFromAST(JSON.parse(txtarea.value));
+		container.innerHTML =
+			`<ul>	
+			<li class="entry toggleable open">
+			<span class="value" id="program">Program</span>
+			<span class="prefix p">${OPEN_BRACE}</span>
+			<ul class="value-body">` +
+			buildHTMLFromAST(JSON.parse(txtarea.value)) +
+			`</ul>
+			<span class="suffix p">${CLOSE_BRACE}</span>
+			</li>
+			</ul>`;
 	},
 	false
 );
@@ -32,13 +42,10 @@ var CLOSE_BRACE = '&#125;';
 var OPEN_BRACKET = '&#91;';
 var CLOSE_BRACKET = '&#93;';
 
-function buildHTMLFromAST(rawAST) {
+function buildHTMLFromAST(rawAST, includeKey) {
 	var html = '<ul>';
-	html += `
-	<li class="entry toggleable open">
-		<span class="value" id="program">Program</span>
-		<span class="prefix p">${OPEN_BRACE}</span>
-		<ul class="value-body">`;
+
+	if (includeKey == undefined) includeKey = true;
 
 	for (var key in rawAST) {
 		if (typeof rawAST[key] == 'object') {
@@ -47,10 +54,33 @@ function buildHTMLFromAST(rawAST) {
 			if (nodeIsArray) {
 				// if there is an array of nodes within
 				// the current node
-				// TODO:
+				html += `
+					<li class="entry toggleable open">
+						<span class="key">
+							<span class="name">${key}</span>
+							<span class="p">:&nbsp;</span>
+						</span>
+						<span class="prefix p">${OPEN_BRACKET}</span>
+						<span class="value">
+							<span class="s">${buildHTMLFromAST(rawAST[key], false)}</span>
+						</span>
+						<span class="suffix p">${CLOSE_BRACKET}</span>
+					</li>
+				`;
 			} else {
 				// if the node is just an object
-				html += buildHTMLFromAST(rawAST[key]);
+				html += `
+					<li class="entry toggleable open">
+						<span class="key">
+							<span class="name">${includeKey ? key : `Element (${key})`}</span>
+							<span class="p">:&nbsp;</span>
+						</span>						
+						<span class="prefix p">${OPEN_BRACE}</span>
+						<span class="value">
+							<span class="s">${buildHTMLFromAST(rawAST[key])}</span>
+						</span>
+						<span class="suffix p">${CLOSE_BRACE}</span>
+					</li>`;
 			}
 		} else {
 			// handle keys with only a simple value
@@ -67,10 +97,6 @@ function buildHTMLFromAST(rawAST) {
 		}
 	}
 
-	html += `
-		</ul>
-		<span class="suffix p">${CLOSE_BRACE}</span>
-	</li>
-</ul>`;
+	html += '</ul>';
 	return html;
 }
