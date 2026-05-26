83ec3bfaf39c5667b52a262eb33a7e8de8372e8d
Bardia Shafaee <shafaeebardia@gmail.com>
2026-05-26 18:46:25 +0000

    added number type

--- src/types/runtime.ts
+++ src/types/runtime.ts
@@ -4,3 +4,13 @@ export interface Value<DataType, Type> {
 	type: DataType;
 	value: Type;
 }
+
+export interface _NUMBER extends Value<'number', number> {}
+
+export function NUMBER(n = 0): _NUMBER {
+	return { type: 'number', value: n } as _NUMBER;
+}
+
+export const DataConstructors = {
+	NUMBER,
+};
